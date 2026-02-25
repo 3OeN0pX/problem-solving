a366b9cc0ebf193228c521bca25a9b64f7a6e5ed
3OeN0pX <3oen0px@gmail.com>
2026-02-25 23:05:56 +0900

    [level 0] Title: l로 만들기, Time: 9.17 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    
+    for ch in myString:
+        if ch < "l":
+            answer += "l"
+        else:
+            answer += ch
+    
+    return answer
