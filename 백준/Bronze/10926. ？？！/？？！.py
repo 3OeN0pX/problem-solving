a6071d36b255e7c6e855ba3e110a090a2b5b9004
3OeN0pX <3oen0px@gmail.com>
2026-03-04 15:50:41 +0900

    [Bronze V] Title: ??!, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution():
+    
+    line = input().strip()
+    line += "??!"
+    
+    print(line)
+    
+if __name__ == "__main__":
+    solution()
