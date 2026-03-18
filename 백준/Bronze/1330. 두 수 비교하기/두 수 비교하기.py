1ff7be450c4eb4bb018eef23df1d3cde485e2a6b
3OeN0pX <3oen0px@gmail.com>
2026-03-18 09:34:38 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+def solve():
+    
+    a, b = map(int, input().split())
+    
+    if a > b:
+        print('>')
+    elif a < b:
+        print('<')
+    elif a == b:
+        print('==')
+        
+if __name__ == '__main__':
+    solve()
